10a4c4d3eab8d32d86980a0596d98f8d445995ad
Tom Manner <zephrincochrane@gmail.com>
2020-05-01 18:22:45 -0400

    Started writing a Board class that can be used to determine measurements and a bom.

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import os
 import platformbed
-from solid import cube, difference, mirror, rotate, scad_render, translate, union
+from solid import cube, difference, mirror, rotate, scad_render_to_file, translate, union
 
 
 if __name__ == "__main__":
     width = 60
     length = 79.5
     overhang = 2
-    with open(f"middle_slat.scad", "w") as scadfile:
-        scadfile.write(scad_render(platformbed.middle_slat(width, overhang)))
-    with open(f"end_slat.scad", "w") as scadfile:
-        scadfile.write(scad_render(platformbed.end_slat(width, overhang)))
-    with open(f"side.scad", "w") as scadfile:
-        scadfile.write(scad_render(platformbed.base_side(length)))
-    with open(f"support.scad", "w") as scadfile:
-        scadfile.write(scad_render(platformbed.base_support(length, overhang)))
-    with open(f"end.scad", "w") as scadfile:
-        scadfile.write(scad_render(platformbed.base_end(width)))
+
+    scad_render_to_file(platformbed.middle_slat(width, overhang), filepath="middle_slat.scad")
+    scad_render_to_file(platformbed.end_slat(width, overhang), filepath="end_slat.scad")
+    scad_render_to_file(platformbed.base_side(length), filepath="base_side.scad")
+    scad_render_to_file(platformbed.base_support(length, overhang), filepath="base_support.scad")
+    scad_render_to_file(platformbed.base_end(width), filepath="base_end.scad")
+    scad_render_to_file(platformbed.base(width, length, overhang), filepath="base.scad")
+    scad_render_to_file(platformbed.slats(width, length, overhang), filepath="slats.scad")
+    scad_render_to_file(platformbed.bed(width, length, overhang), filepath="bed.scad")
--- platformbed/__init__.py
+++ platformbed/__init__.py
@@ -1,4 +1,4 @@
-from solid import cube, hole, mirror, part, rotate, translate
+from solid import color, cube, hole, mirror, part, rotate, translate, union, OpenSCADObject
 from .joints import dovetail
 
 X = 0
@@ -7,9 +7,6 @@ Z = 2
 
 
 def board(length, width, thickness, axis):
-    length = length
-    width = width
-    thickness = thickness
     return cube(tuple(s[0] for s in sorted(
         zip(
             (length, width, thickness),
@@ -31,6 +28,34 @@ def two_by_four(length, axis=(X, Y, Z)):
     return board(length, 3.5, 3/2, axis)
 
 
+class Board:
+    def __init__(self, length, width, thickness, axis=(X, Y, Z)):
+        super().__init__("Board", {})
+        self.length = length
+        self.width = width
+        self.thickness = thickness
+        self.axis = axis
+
+    def scad(self):
+        return board(self.length, self.width, self.thickness, self.axis)
+
+
+class TwoByFour(Board):
+    WIDTH = 3.5
+    THICKNESS = 1.5
+    def __init__(self, length, axis=(X, Y, Z)):
+        super().__init__(length, , axis)
+
+
+def slat_displacements(matress_length):
+    step = 7
+    offset = 7/4 + ((matress_length - 7) % step) / 2
+    displacement = 3.5
+    while displacement < (matress_length - step):
+        yield offset + displacement
+        displacement += step
+
+
 def middle_slat(matress_width, overhang):
     length = matress_width + overhang * 2
     half_cross = hole()(cube((3/2, 3.5, 0.25)))
@@ -43,50 +68,77 @@ def middle_slat(matress_width, overhang):
 
 def end_slat(matress_width, overhang):
     length = matress_width + overhang * 2
-    half_cross = hole()(cube((3/2, 5.5, 0.5)))
+    half_cross = hole()(cube((3/2, 3.5, 0.5)))
     mortice = hole()(cube((3/2, 3/2, 3/4)))
     return part()(
         one_by_six(length)
-      - translate((length - 3/2 - overhang, 0, 0))(half_cross)
-      - translate((overhang, 0, 0))(half_cross)
-      - translate((matress_width / 3 - 3/4, 2, 0))(mortice)
-      - translate((2 * matress_width / 3 - 3/4, 2, 0))(mortice)
+      - translate((length - 3/2 - overhang, 2, 1/4))(half_cross)
+      - translate((overhang, 2, 1/4))(half_cross)
+      - translate((overhang + matress_width / 3 - 3/4, 2, 0))(mortice)
+      - translate((overhang + 2 * matress_width / 3 - 3/4, 2, 0))(mortice)
     )
 
 
 def base_corner_joint():
     return (
         translate((3/2, 0, 1 + 1/4 + 3/4))(rotate((0, 90, 90))(dovetail(1.25, 3/2, 3/2, 1/6)))
-      + translate((0, 0, 3))(cube((3/2, 3/2, 0.5)))
+      + translate((0, 0, 3))(cube((3/2, 3/2, 1/2)))
+      - hole()(translate((0, 3/2, 11/4))(cube((3/2, 3/2, 1/4))))
     )
 
 
-def base_side(matress_length):#, num_slats, slat_width):
-    return part()(
+def base_side(matress_length):
+    half_cross = hole()(cube((3/2, 7/2, 1/2)))
+    return color("tan")(part()(
         translate((0, 3/2, 0))(two_by_four(matress_length - 3, (Y, Z, X)))
       + base_corner_joint()
       + translate((0, matress_length - 3/2, 0))(base_corner_joint())
-    )
+      - union()(*[translate((0, y, 3))(half_cross) for y in slat_displacements(matress_length)])
+    ))
 
 
 def base_support(matress_length, overhang):
     half_cross = hole()(cube((3/2, 3/2, 11/8)))
-    return part()(
+    return color("tan")(part()(
         board(matress_length + overhang * 2, 11/4, 3/2, (Y, Z, X))
       - translate((0, overhang, 11/8))(half_cross)
       - translate((0, overhang + matress_length - 3/2, 11/8))(half_cross)
-    )
+    ))
 
 
 def base_end(matress_width):
     joint = hole()(base_corner_joint())
     tenon = cube((3/2, 3/2, 3/4))
-    return part()(
+    half_cross = hole()(cube((3/2, 3/2, 11/8)))
+    return color("saddlebrown")(part()(
         board(matress_width, 11/4, 3/2, (X, Z, Y))
       - joint
       - translate((matress_width, 0, 0))(mirror((1, 0, 0))(joint))
       + translate((matress_width / 3 - 3/4, 0, 11/4))(tenon)
       + translate((2 * matress_width / 3 - 3/4, 0, 11/4))(tenon)
+      - translate((matress_width / 3 - 3/4, 0, 0))(half_cross)
+      - translate((2 * matress_width / 3 - 3/4, 0, 0))(half_cross)
+    ))
+
+
+def slats(matress_width, matress_length, overhang):
+    return color("peru")(
+        translate((0, 0, 11/4))(end_slat(matress_width, overhang))
+      + translate((0, matress_length + 2 * overhang, 11/4))(mirror((0, 1, 0))(end_slat(matress_width, overhang)))
+      + union()(*[translate((0, overhang + y, 11/4))(middle_slat(matress_width, overhang)) for y in slat_displacements(matress_length)])
     )
 
 
+def base(matress_width, matress_length, overhang):
+    return (
+        translate((0, overhang, 0))(base_end(matress_width))
+      + translate((0, overhang + matress_length, 0))(mirror((0, 1, 0))(base_end(matress_width)))
+      + translate((0, overhang, 0))(base_side(matress_length))
+      + translate((matress_width, overhang, 0))(mirror((1, 0, 0))(base_side(matress_length)))
+      + translate((matress_width / 3 - 3/4, 0, 0))(base_support(matress_length, overhang))
+      + translate((2 * matress_width / 3 - 3/4, 0, 0))(base_support(matress_length, overhang))
+    )
+
+
+def bed(matress_width, matress_length, overhang):
+    return translate((overhang, 0, 0))(base(matress_width, matress_length, overhang)) + slats(matress_width, matress_length, overhang)
